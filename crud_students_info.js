ef7792f131fc9b5fd66934bd04c7ec22aa017f95
2bXorNot <64798261+Ravi-Maia@users.noreply.github.com>
2023-05-18 15:30:08 -0300

    In the find function, use Object.values method to return the object values as an array to use thefind method; delete DataStudArray array and the find methods in the find function that look for the desired values in the array; delete the for loop and for ... in loop that search for the value to be modified and to modify said value in the deleted array, and use two for ... in loops to find and modify the desired value in the object DataStudents

--- crud_students_info.js
+++ crud_students_info.js
@@ -1,31 +1,25 @@
-
+console.log("running");
 let DataStudents = {};
 console.log(DataStudents);
-
 let classNum = 0;
-let DaraStudArr = [];
-console.log(DaraStudArr);
 let classNumContainer = new Map();
 console.log(classNumContainer);
 
-const add = () => {DataStudents[classNum] = {name: '', age: 0, email:''}};
+const add = () => {DataStudents[classNum] = {"name": '', age: 0, email:''}};
 add();
 
 const addContent = () => {insertData()};
 
-const insertData = () => {
-    
+const insertData = () => {    
     let studentName = document.getElementById('stud-name');
     let studentAge = document.getElementById('age');
     let studentEmail = document.getElementById('email');
     if(studentName.value === '') {
-        console.log('cannot leave name empty');
-        
+        console.log('cannot leave name empty');        
     } else {
         classNumContainer.set(DataStudents[classNum].name = studentName.value, classNum);
         console.log(classNumContainer.get(studentName.value));        
     };
-
     if(studentAge.value === '' ) { 
         console.log('cannot leave age empty');
          
@@ -34,8 +28,7 @@ const insertData = () => {
         classNumContainer.set(DataStudents[classNum].age = studentAge.value, classNum);
     };
     if(studentEmail.value === '') { 
-        console.log('cannot leave email empty');
-         
+        console.log('cannot leave email empty');   
     } else {
         DataStudents[classNum].email = studentEmail.value;
         classNumContainer.set(DataStudents[classNum].email = studentEmail.value, classNum);
@@ -43,56 +36,35 @@ const insertData = () => {
     
     if(studentName.value && studentAge.value && studentEmail.value) {
         classNumContainer.set(DataStudents[classNum].id = Math.random(2).toFixed(2) * 1000);
-        DaraStudArr.push((DataStudents[classNum]));
         add2();      
     };
 };
 
 const add2 = () => {classNum++; DataStudents[classNum] = {name: '', age: 0, email: ''}; document.getElementById('stud-name').value = ''; document.getElementById('age').value = ''; document.getElementById('email').value = '';};
 
-let find2;
 function find() {
-    find2 = document.getElementById('find');
-    let foundName = DaraStudArr.find(({name}) => name === find2.value);
-    let foundIdade = DaraStudArr.find(({age}) => age === find2.value);
-    let foundEmail = DaraStudArr.find(({email}) => email === find2.value);
-    let foundId = DaraStudArr.find(({id}) => Number(id) === Number(find2.value)); 
-
-    if(typeof foundIdade !== 'undefined' && typeof foundName !== 'undefined' && typeof foundId !== 'undefined') {
-        if(foundName.name !== find2.value) {
-            console.log('This name does not exist');
-        } else {
-            console.log(foundName.name);
-        };
-        if(foundIdade.age === find2.value) {
-            console.log(foundIdade.age);
-        };
-        if(foundEmail.email === find2.value) {
-            console.log(foundEmail.email);
-        };
-        if(Number(foundId.id) === Number(find2.value)) {
-            console.log(foundId.id);
-        };
-        
-    };
+    let find2 = document.getElementById('find');
+    let foundName = Object.values(DataStudents).find(value => value.name === find2.value);
     console.log(foundName);
+    let foundIdade = Object.values(DataStudents).find(value => value.age === find2.value);
     console.log(foundIdade);
+    let foundEmail = Object.values(DataStudents).find(value => value.email === find2.value);
     console.log(foundEmail);
+    let foundId = Object.values(DataStudents).find(value => value.id === find2.value);
     console.log(foundId);
     document.getElementById('find').value = '';
 };
 
 let updateInfo
 const modifyContent = () => {
-    updateInfo = document.getElementById('update');
-    let enterNewData = prompt('New data');
-    for(let i = 0; i < DaraStudArr.length; i++) {
-        document.getElementById('age').value = '';
-        for(keys in DaraStudArr[i]) {
-            if(DaraStudArr[i][keys] === updateInfo.value) {
-                DaraStudArr[i][keys] = enterNewData;
-                console.log(DataStudents);
-           };
+    updateInfo = document.getElementById('update');    
+    let enterNewData = prompt('New data');    
+    for(keys in DataStudents) {
+        let values = DataStudents[keys]
+        for(keys in values) {
+            if(values[keys] === updateInfo.value) {
+                values[keys] = enterNewData;
+            };
         };
     };
     document.getElementById('update').value = '';
@@ -102,9 +74,7 @@ let deleteInfo;
 function deleteContent() {
     deleteInfo = document.getElementById('delete');
     console.log(classNumContainer.get(deleteInfo.value));
-    delete DaraStudArr[classNumContainer.get(deleteInfo.value)];
     console.log(DataStudents)
     delete DataStudents[classNumContainer.get(deleteInfo.value)];
-
     document.getElementById('delete').value = '';
 };
